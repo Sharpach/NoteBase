1c91039cf69a37f04612d2a9bef911a88d69ae52
Vlad Kabalov <vladskryto@gmail.com>
2018-02-02 13:40:08 +0400

    Add project files.

---
+++ IRO.Task.NoteBase.PL/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRO.Task.NoteBase.PL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
