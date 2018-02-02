fbaa1a25d7dfc71aca53e137e116a5a2b06b33d2
admiralWoop <danchic2202@gmail.com>
2018-02-02 18:21:26 +0700

    Added BLL.Contracts

---
+++ IRO.Task.NoteBase.BLL.Contracts/IUserLogic.cs
@@ -0,0 +1,11 @@
+﻿using IRO.Task.NoteBase;
+using System.Collections.Generic;
+
+namespace IRO.Task.NoteBase.BLL.Contracts
+{
+    public interface IUserLogic
+    {
+        bool AddUser(User user);
+        List<User> List();
+    }
+}
