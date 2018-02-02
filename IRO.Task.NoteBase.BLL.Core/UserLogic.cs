b05131fdf1ed519bf785875629bda5d50a802f88
Vlad Kabalov <vladskryto@gmail.com>
2018-02-02 15:43:15 +0400

    Add references

--- IRO.Task.NoteBase.BLL.Core/UserLogic.cs
+++ IRO.Task.NoteBase.BLL.Core/UserLogic.cs
@@ -1,12 +1,19 @@
-﻿using IRO.Task.NoteBase.BLL.Contracts;
-using IRO.Task.NoteBase;
+﻿using System.Collections.Generic;
+using IRO.Task.NoteBase.BLL.Contracts;
+using IRO.Task.NoteBase.Entities;
+
 namespace IRO.Task.NoteBase.BLL.Core
 {
     public class UserLogic : IUserLogic
     {
         public bool AddUser(User user)
         {
+            return false;
+        }
 
+        public List<User> List()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
