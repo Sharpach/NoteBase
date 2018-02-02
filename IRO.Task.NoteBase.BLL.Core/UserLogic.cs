c69930e6a803c76587998a489202259c31de3389
Vlad Kabalov <vladskryto@gmail.com>
2018-02-02 18:05:16 +0400

    Add logic

--- IRO.Task.NoteBase.BLL.Core/UserLogic.cs
+++ IRO.Task.NoteBase.BLL.Core/UserLogic.cs
@@ -1,23 +1,39 @@
 ﻿using System.Collections.Generic;
 using IRO.Task.NoteBase.BLL.Contracts;
+using IRO.Task.NoteBase.DAL.Contracts;
+using IRO.Task.NoteBase.DAL.Memory;
 using IRO.Task.NoteBase.Entities;
 
 namespace IRO.Task.NoteBase.BLL.Core
 {
     public class UserLogic : IUserLogic
     {
+        private readonly IUserDao _userDao;
         public User ActiveUser { get; private set; }
+
+        public UserLogic()
+        {
+            _userDao = new UserDao();
+        }
+
         public bool AddUser(User user)
         {
-            return false;
+            return _userDao.AddUser(user);
         }
+
         public bool Login(uint userId)
         {
-            return false;
+             var loggedUser = _userDao.GetUserById(userId);
+            if (loggedUser == null)
+                return false;
+            ActiveUser = loggedUser;
+            return true;
+
         }
+
         public List<User> List()
         {
-            throw new System.NotImplementedException();
+            return _userDao.GetUsersList();
         }
     }
 }
