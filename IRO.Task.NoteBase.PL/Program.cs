9ed898b733fe8b2c04e17dc52efdf96aa5ea612f
Vlad Kabalov <vladskryto@gmail.com>
2018-02-02 14:08:57 +0400

    Init commit

--- IRO.Task.NoteBase.PL/Program.cs
+++ IRO.Task.NoteBase.PL/Program.cs
@@ -6,10 +6,11 @@ using System.Threading.Tasks;
 
 namespace IRO.Task.NoteBase.PL
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+
         }
     }
 }
