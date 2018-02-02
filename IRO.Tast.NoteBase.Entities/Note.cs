969c66506427404a5511ed3451fa74a81e33796a
admiralWoop <danchic2202@gmail.com>
2018-02-02 17:57:14 +0700

    Added new entitie.

---
+++ IRO.Tast.NoteBase.Entities/Note.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRO.Tast.NoteBase.Entities
+{
+    public class Note
+    {
+        public uint ID;
+        public string Text;
+
+    }
+}
