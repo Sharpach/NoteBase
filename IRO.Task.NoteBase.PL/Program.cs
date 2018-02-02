b9a3a1dbdfeeb2c5f32adaa93fe9247eb1d74019
Vlad Kabalov <vladskryto@gmail.com>
2018-02-02 15:10:41 +0400

    PL edit

--- IRO.Task.NoteBase.PL/Program.cs
+++ IRO.Task.NoteBase.PL/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IRO.Task.NoteBase.PL
 {
@@ -10,7 +6,61 @@ namespace IRO.Task.NoteBase.PL
     {
         private static void Main(string[] args)
         {
-
+            do
+            {
+                DisplayCommands();
+                switch (Console.ReadLine())
+                {
+                    case "AddUser":
+                        {
+                            Console.Clear();
+                            break;
+                        }
+                    case "Login":
+                        {
+                            Console.Clear();
+                            break;
+                        }
+                    case "List":
+                        {
+                            Console.Clear();
+                            break;
+                        }
+                    case "AddNote":
+                        {
+                            Console.Clear();
+                            break;
+                        }
+                    case "ShowAllNotes":
+                        {
+                            Console.Clear();
+                            break;
+                        }
+                    case "Books":
+                        {
+                            Console.Clear();
+                            break;
+                        }
+                    default:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Ошибка выбора!");
+                            break;
+                        }
+                }
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Q);
         }
+
+        private static void DisplayCommands()
+        {
+            Console.WriteLine("AddUser - Добавление пользователя в программу\n" +
+                              "Login - вход под пользователем из списка\n" +
+                              "List - вывести имена всех пользователей\n" +
+                              "AddNote - добавить записку в книгу\n" +
+                              "ShowAllNotes - вывести все записки из книги\n" +
+                              "Books - вывести ID всех книг");
+        }
+
     }
 }
