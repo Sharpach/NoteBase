b04fa5e18533cd01684080dea85dbc88f557d664
admiralWoop <danchic2202@gmail.com>
2018-02-02 19:22:23 +0700

    Added IUserDAO

---
+++ IRO.Task.NoteBase.DAL.Contracts/IUserDAO.cs
@@ -0,0 +1,11 @@
+﻿using IRO.Task.NoteBase.Entities;
+
+namespace IRO.Task.NoteBase.DAL.Contracts
+{
+    public interface IUserDAO
+    {
+        bool AddUser(User user);
+        bool GetUserByID(uint userID);
+        bool GetUsersList();
+    }
+}
